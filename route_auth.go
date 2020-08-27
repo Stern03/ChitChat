e312a9ed12df9d7f5be9e1d5b378049ea9d0e4fd
Stern03 <aki.kimura0929@gmail.com>
2020-08-27 23:47:37 +0900

    add files

---
+++ route_auth.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func authenticate(w http.ResponseWriter, r * http.Request)  {
+	r.ParseForm()
+	user, _ := data.UserByEmail(r.PostFormValue("email"))
+	if user.Password == data.Encrypt(r.PostFormValue("password")) {
+		session := user.CreateSession()
+		cookie := http.Cookie{
+			Name: "_cookie",
+			Value: session.Uuid,
+			HttpOnly: true,
+		}
+	}
+}
